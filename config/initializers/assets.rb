87b70be6510da7cf777887c2b65c9c490e7c6e0e
Badjo <bart@badjo.nl>
2023-07-11 11:24:54 +0200

    remocved scss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,3 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 
 # Rails.application.config.assets.precompile += %w( application.scss )
-Rails.application.config.assets.precompile += %w( application.scss bootstrap.min.js popper.js )
