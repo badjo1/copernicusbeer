a9b8c5ecb71ffd0a5b0c39198bee87f976c319be
Badjo <bart@badjo.nl>
2024-06-18 13:40:59 +0200

    fixed stimulus

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,7 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "local-time" # @2.1.0
