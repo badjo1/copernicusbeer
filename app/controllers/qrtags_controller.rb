cfa9afcd330a3fe688e3781446ade1117bc35b9f
Badjo <bart@badjo.nl>
2024-11-21 12:09:27 +0100

    new csv for new printer

--- app/controllers/qrtags_controller.rb
+++ app/controllers/qrtags_controller.rb
@@ -31,24 +31,58 @@ class QrtagsController < ProtectedController
   end
 
   # GET /labels/:id/qrtags 
+  # def index
+  #   custom_order = [4, 3, 2, 1, 8, 7, 6, 5, 4, 12, 11, 10, 9, 16, 15, 14, 13, 9, 8, 20, 19, 18, 17, 16, 24, 23, 22, 21]
+  #   order_case = custom_order.each_with_index.map { |num, index| "WHEN #{num} THEN #{index}" }.join(' ')
+
+  #   @label = Label.find params[:label_id]
+  #   @qrtags = @label.qrtags.joins(:qrcode).includes(:qrcode).order(:labelnumber).order(Arel.sql("CASE referencenumber #{order_case} END"))
+
+  #   csv_name = "qrtags-#{@label.batch.serialnumber}-#{@label.code}-#{Date.today}.csv"   
+  #   respond_to do |format|
+  #     # format.html
+  #     format.csv { send_data @qrtags.to_csv, filename: csv_name }
+  #   end 
+  # end
+
   def index
-    custom_order = [4, 3, 2, 1, 8, 7, 6, 5, 4, 12, 11, 10, 9, 16, 15, 14, 13, 9, 8, 20, 19, 18, 17, 16, 24, 23, 22, 21]
-    order_case = custom_order.each_with_index.map { |num, index| "WHEN #{num} THEN #{index}" }.join(' ')
+    @label = Label.find_by(id: params[:label_id])
+    unless @label
+      render plain: "Label not found", status: :not_found and return
+    end
 
-    @label = Label.find params[:label_id]
-    @qrtags = @label.qrtags.joins(:qrcode).includes(:qrcode).order(:labelnumber).order(Arel.sql("CASE referencenumber #{order_case} END"))
+    @qrtags = @label.qrtags
+                    .joins(:qrcode)
+                    .order(:labelnumber, :referencenumber)
 
-    csv_name = "qrtags-#{@label.batch.serialnumber}-#{@label.code}-#{Date.today}.csv"   
+    # Groepeer de qrtags per labelnummer
+    @grouped_qrtags = @qrtags.group_by(&:labelnumber)
+
+    # Bestandsnaam maken
+    csv_name = "qrtags-#{@label.batch.serialnumber}-#{@label.code}-#{Date.today.iso8601}.csv"
+
+    # Reageren op CSV-aanvragen
     respond_to do |format|
-      # format.html
-      format.csv { send_data @qrtags.to_csv, filename: csv_name }
-    end 
+      format.html # Renders a view (optional, for browser access)
+      format.csv { send_data generate_csv(@grouped_qrtags), filename: csv_name }
+    end
   end
+
 
   private
 
     def qrtags_params
       params.require(:qrtag).permit(:q)
+    end
+
+    # CSV te genereren
+    def generate_csv(grouped_qrtags)
+      CSV.generate(headers: false) do |csv|
+        # csv << ["Label Number", "QR Codes"] # CSV-kopteksten
+        grouped_qrtags.each do |labelnumber, tags|
+          csv << tags.map { |tag| tag.tagurl }
+        end
+      end
     end
 
 end
