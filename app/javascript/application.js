ffaae8aca47bba68ad2fafc5b389159e350a32a6
Badjo <bart@badjo.nl>
2025-03-28 13:18:58 +0100

    updated ruby, rails en other gems, confirmation on delete

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
 import LocalTime from "local-time"
+console.log('init copernicus beer app')
 LocalTime.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,11 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tailwindcss-stimulus-components" # @6.1.3
 pin "local-time" # @2.1.0
+
+
+
