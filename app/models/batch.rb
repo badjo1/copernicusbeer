ac984e69663a7ecfd73699c4c39ad13cdae2ac5a
Badjo <bart@badjo.nl>
2025-04-01 11:52:51 +0200

    update importmap

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,6 +1,7 @@
 class Batch < ApplicationRecord
-	validates_presence_of :serialnumber, :description
 	has_many :labels, dependent: :destroy
+	validates_presence_of :serialnumber, :description
+	validates :serialnumber	, numericality: { only_integer: true }, allow_blank: true
 
 	def name
 		"Batch #{serialnumber}"
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,11 +2,11 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/stimulus", to: "stimulus.min.js" 
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tailwindcss-stimulus-components" # @6.1.3
-pin "local-time" # @2.1.0
+pin "local-time" # @3.0.3
 
 
 
